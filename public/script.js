089fa6f20551a4ceca80d9445168974a85045843
Vazha20 <gabisoniavazha@gmail.com>
2024-04-26 19:22:29 +0400

    ohhh

---
+++ public/script.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCALcgnrAT96DdZVmxroSe3lPe5rxwnewA",
+  authDomain: "beprogeo-5b650.firebaseapp.com",
+  databaseURL: "https://beprogeo-5b650-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "beprogeo-5b650",
+  storageBucket: "beprogeo-5b650.appspot.com",
+  messagingSenderId: "311927512328",
+  appId: "1:311927512328:web:e522cf2acf48afb052a0bd",
+  measurementId: "G-BXT2E91K62"
+};
